bc2b3e87dec52c3fd120aa57a104b61f5c6dd721
HutsonJason <110316760+HutsonJason@users.noreply.github.com>
2023-01-10 20:41:47 -0500

    Testing pre-commit

--- src/power_bill_extractor/bill.py
+++ src/power_bill_extractor/bill.py
@@ -1,5 +1,10 @@
+import click
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextBoxHorizontal
 
+# Test commit
+x = 1
+y = 2
 
-...
+if x + y == 3:
+    click.echo(click.style("Yay it works!", fg="green"))
